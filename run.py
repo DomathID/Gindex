2e17dd140fb074c28acb71cef45715113279e553
Bang Domath ID <admin@yukinoshita.web.id>
2023-10-03 02:15:07 +0700

    Update run.py
    Add Mass Submit from Sitemap.xml

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import httplib2
 import json
 from colorama import Fore, Style, init
 import os
+from xml.dom import minidom
+import requests
 
 init()
 
@@ -22,6 +24,7 @@ try:
 except Exception as e:
     print(f"{Fore.RED}{Style.BRIGHT}Terjadi kesalahan dalam memuat credentials.json. Pastikan file tersebut berisi kredensial yang benar.{Style.RESET_ALL}")
     exit()
+
 def delete_url(url):
     content = {
         'url': url,
@@ -60,6 +63,18 @@ def print_success(message):
 def print_error(message):
     print(f"{Fore.RED}{Style.BRIGHT}{message}{Style.RESET_ALL}")
 
+def get_urls_from_sitemap(sitemap_url):
+    try:
+        response = requests.get(sitemap_url)
+        response.raise_for_status()
+        xml_content = response.content
+        dom = minidom.parseString(xml_content)
+        urls = [url.firstChild.nodeValue for url in dom.getElementsByTagName('loc')]
+        return urls
+    except Exception as e:
+        print_error(f"Terjadi kesalahan saat mengambil URL dari sitemap: {e}")
+        return []
+
 def get_user_input():
     banner = '''
     ⠀⠀⠀⠀⠀⠀  ⠀⢀⠀⠀⠀⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -78,8 +93,9 @@ def get_user_input():
     print("=== Menu Alat ===")
     print("1. Kirim URL")
     print("2. Hapus URL")
+    print("3. Mass Submit URL")
     print("0. Keluar")
-    choice = input("Pilih alat (0-2): ")
+    choice = input("Pilih alat (0-3): ")
     url = ""
     if choice == "1":
         url = input("Masukkan URL yang ingin diindeks: ")
@@ -87,14 +103,22 @@ def get_user_input():
     elif choice == "2":
         url = input("Masukkan URL yang ingin dihapus: ")
         delete_url(url)
+    elif choice == "3":
+        sitemap_url = input("Masukkan URL sitemap: ")
+        urls = get_urls_from_sitemap(sitemap_url)
+        if urls:
+            for url in urls:
+                send_url(url)
+        else:
+            print_error("Tidak dapat mengambil URL dari sitemap.")
     elif choice == "0":
         return False
     else:
         print_error("Pilihan tidak valid.")
     return True
 
-
 if __name__ == "__main__":
     while get_user_input():
         pass
+
 
